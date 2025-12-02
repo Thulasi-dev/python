7ae1465941071d8fc6e77cae8d61aabc364be83b
Thulasi <83580674+Thulasi-dev@users.noreply.github.com>
2025-12-02 19:54:54 +0530

    Create lengthOfLongestSubstring.py

---
+++ lengthOfLongestSubstring.py
@@ -0,0 +1,19 @@
+// Given a string s, find the length of the longest substring without duplicate characters.
+
+def lengthOfLongestSubstring(s):
+    char_index = {}
+    left = 0
+    max_len = 0
+
+    for right in range(len(s)):
+        if s[right] in char_index and char_index[s[right]] >= left:
+            left = char_index[s[right]] + 1
+        
+        char_index[s[right]] = right
+        max_len = max(max_len, right - left + 1)
+
+    return max_len
+
+
+// Input: s = "abcabcbb"
+// Output: 3
