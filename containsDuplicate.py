c2c41ab7f5bcf02584a7932306f1d0cc664727d6
Thulasi <83580674+Thulasi-dev@users.noreply.github.com>
2025-12-04 15:45:01 +0530

    Create containsDuplicate.py

---
+++ containsDuplicate.py
@@ -0,0 +1,26 @@
+// Contains Duplicate
+
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+def containsDuplicate(nums):
+    seen = set()
+
+    for num in nums:
+        if num in seen:      # duplicate found
+            return True
+        seen.add(num)
+
+    return False             # no duplicates
+
+
+"""Input: nums = [1,2,3,1]
+Output: true
+
+Input: nums = [1,2,3,4]
+Output: false"""
+
+
+
+
+
+
